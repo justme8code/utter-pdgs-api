5529ece5372bf869a0d7c06fbd5245e24debc1c4
justme8code <jvmMonster1@gmail.com>
2025-03-24 11:41:42 +0100

    Update

--- src/test/java/com/justme8code/utterfresh_production_gathering_sys/UtterfreshProductionGatheringSysApplicationTests.java
+++ src/test/java/com/justme8code/utterfresh_production_gathering_sys/UtterfreshProductionGatheringSysApplicationTests.java
@@ -2,9 +2,11 @@ package com.justme8code.utterfresh_production_gathering_sys;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @ContextConfiguration(classes = UtterfreshProductionGatheringSysApplication.class)
 class UtterfreshProductionGatheringSysApplicationTests {
 
